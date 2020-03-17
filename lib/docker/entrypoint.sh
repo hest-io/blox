f1b832f7aab911635207e0d40b688174265308b1
Hugh Mooney <hugh.mooney@gmail.com>
2020-03-17 11:33:58 +0000

    Re-ordered init to ensure default TF version

--- lib/docker/entrypoint.sh
+++ lib/docker/entrypoint.sh
@@ -3,12 +3,12 @@
 # Defaults
 : ${DEFAULT_TERRAFORM_VERSION:="0.11.3"}
 
-# Link the default TF version
-ln -s "/usr/local/bin/terraform-${DEFAULT_TERRAFORM_VERSION}" "${HOME}/bin/terraform"
-
 # UID/GID (may) map to unknown user/group, $HOME=/ (the default when no home directory is defined)
 eval $( fixuid -q )
 # UID/GID now match user/group, $HOME has been set to user's home directory
+
+# Link the default TF version
+ln -s "/usr/local/bin/terraform-${DEFAULT_TERRAFORM_VERSION}" "${HOME}/bin/terraform"
 
 # On with the show
 $@
