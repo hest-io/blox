94afc59582cbbf93126f934b65009633366bdb50
Hugh Mooney <hugh.mooney@gmail.com>
2020-03-26 12:02:52 +0000

    Added workaround for another GitLab CICD issue where it executes the ENTRYPOINT once for each stage in a CICD pipeline

--- lib/docker/entrypoint.sh
+++ lib/docker/entrypoint.sh
@@ -7,7 +7,9 @@
 eval $( fixuid -q )
 # UID/GID now match user/group, $HOME has been set to user's home directory
 
-# Link the default TF version
+# Link the default TF version. Remove existing file/link if exists as a workaround
+# to GitLab CICD issue where entrypoint is run for each stage when using container based builds
+[[ -f "${HOME}/bin/terraform" ]] && rm -f "${HOME}/bin/terraform"
 ln -s "/usr/local/bin/terraform-${DEFAULT_TERRAFORM_VERSION}" "${HOME}/bin/terraform"
 
 # On with the show
