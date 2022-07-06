1221b82e9edf4518e2aca27e03d7c9113aa78c13
Hugh Mooney <hugh.mooney@gmail.com>
2022-07-06 17:14:43 +0100

    Updated config for Starship usage

--- lib/docker/entrypoint.sh
+++ lib/docker/entrypoint.sh
@@ -7,6 +7,10 @@
 eval $( fixuid -q )
 # UID/GID now match user/group, $HOME has been set to user's home directory
 
+# Starship setup
+[ -d "${HOME}/.config" ] || mkdir -p "${HOME}/.config"
+[ -f "${HOME}/.config/starship.toml" ] || ln -s /opt/awsh/lib/starship/starship.toml "${HOME}/.config/starship.toml"
+
 # Link the default TF version. Remove existing file/link if exists as a workaround
 # to GitLab CICD issue where entrypoint is run for each stage when using container based builds
 [[ -f "${HOME}/bin/terraform" ]] && rm -f "${HOME}/bin/terraform"
